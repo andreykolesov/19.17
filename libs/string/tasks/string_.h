e28773d0675dbeb1e41db0bbac1a8ece28033ef1
andreykolesov <1yasnoclown1@gmail.com>
2024-04-13 23:01:03 +0300

    copy functions for strings (copy, copyIf, copyIfReverse)

--- libs/string/tasks/string_.h
+++ libs/string/tasks/string_.h
@@ -3,6 +3,7 @@
 
 #include <stdlib.h>
 #include <ctype.h>
+#include <memory.h>
 
 // возвращает количество символов в строке (не считая ноль-символ)
 size_t strlen_(const char *begin);
@@ -31,5 +32,18 @@ char *findSpaceReverse(char *r_begin, const char *r_end);
 // Функция возвращает отрицательное значение, если lhs располагается до rhs в лексикографическом
 // порядке (как в словаре), значение 0, если lhs и rhs равны, иначе – положительное значение
 int strcmp(const char *lhs, const char *rhs);
+
+// записывает по адресу beginDestination фрагмент памяти, начиная с адреса beginSource до endSource.
+// Возвращает указатель на следующий свободный фрагмент памяти в destination.
+char *copy(const char *beginSource, const char *endSource, char *beginDestination);
+
+// записывает по адресу beginDestination элементы из фрагмента памяти начиная с beginSource заканчивая endSource,
+// удовлетворяющие функции-предикату f. Функция возвращает указатель на
+// следующий свободный для записи фрагмент в памяти.
+char* copyIf(char *beginSource, const char *endSource, char *beginDestination, int (*f)(int));
+
+// записывает по адресу beginDestination элементы из фрагмента памяти начиная с r_beginSource заканчивая r_endSource,
+// удовлетворяющие функции-предикату f. Функция возвращает значение beginDestination по окончанию работы функции
+char* copyIfReverse(char *r_beginSource, const char *r_endSource, char *beginDestination, int (*f)(int));
 
 #endif
--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include "libs/string/tasks/string_.h"
 #include <assert.h>
 
+
 void test_find_1_char_in_string() {
     char *str = "Hello, World!";
 
@@ -88,7 +89,7 @@ void test_findNonSpaceReverse_1_spaces_in_string() {
     char *str = "Hello, World!    ";
     char *res = findNonSpaceReverse(str + strlen_(str) - 1, str);
 
-    assert(res == &str[12]);
+    assert(*res == '!');
 }
 
 void test_findNonSpaceReverse_2_no_spaces_in_string() {
@@ -132,7 +133,7 @@ void test_findSpaceReverse_3_string_with_spaces() {
     assert(res == &str[1]);
 }
 
-void test_findSpaceReverse(){
+void test_findSpaceReverse() {
     test_findSpaceReverse_1_spaces_in_string();
     test_findSpaceReverse_2_no_spaces_in_string();
     test_findSpaceReverse_3_string_with_spaces();
@@ -162,10 +163,99 @@ void test_strcmp_3_second_string_larger() {
     assert(res < 0);
 }
 
-void test_strcmp(){
+void test_strcmp() {
     test_strcmp_1_equal_strings();
     test_strcmp_2_first_string_larger();
     test_strcmp_3_second_string_larger();
+}
+
+void test_copy_1_copy_non_empty_string() {
+    char *str = "Hello, World!";
+    char destination[15];
+
+    char *res = copy(str, str + strlen_(str), destination);
+
+    for (int i = 0; i < strlen_(str); i++)
+        assert (destination[i] == str[i]);
+
+    assert(res == destination + strlen_(str));
+}
+
+void test_copy_2_copy_empty_string() {
+    char *str = "";
+
+    char destination[50] = "Hello, World!";
+
+    char *result = copy(str, str, destination);
+
+    for (int i = 0; i < strlen_(str); i++)
+        assert (destination[i] == str[i]);
+
+    assert(result == destination);
+}
+
+void test_copy_3_copy_substring() {
+    const char *str = "Hello, World!";
+    char destination[50];
+
+    char *result = copy(str, str + 5, destination);
+
+    for (int i = 0; i < 5; i++)
+        assert (destination[i] == str[i]);
+
+    assert(result == destination + 5);
+}
+
+void test_copy() {
+    test_copy_1_copy_non_empty_string();
+    test_copy_2_copy_empty_string();
+    test_copy_3_copy_substring();
+}
+
+void test_copyIf_1_digit_copying() {
+    char *str = "Hello123World456";
+    char destination[50];
+    char *result = copyIf(str, str + strlen_(str), destination, isdigit);
+
+    assert(strcmp(destination, "123456") == 0);
+    assert(result == destination + 6);
+}
+
+void test_copyIf_2_letter_copying() {
+    char *str = "Hello123World456";
+    char destination[50];
+    char *result = copyIf(str, str + strlen_(str), destination, isalpha);
+
+    assert(strcmp(destination, "HelloWorld") == 0);
+    assert(result == destination + 10);
+}
+
+void test_copyIf() {
+    test_copyIf_1_digit_copying();
+    test_copyIf_2_letter_copying();
+}
+
+void test_copyIfReverse_1_digit_copying(){
+    char *str = "341224abcde";
+    char destination[50] = {0};
+    char *result = copyIfReverse(str + strlen_(str) - 1, str - 1, destination, isalpha);
+
+    assert(strcmp(destination, "edcba") == 0);
+    assert(result == destination + 5);
+}
+
+void test_copyIfReverse_2_letter_copying(){
+    char *str = "341224abcde";
+    char destination[50] = {0};
+    char *result = copyIfReverse(str + strlen_(str) - 1, str - 1, destination, isdigit);
+
+    assert(strcmp(destination, "422143") == 0);
+    assert(result == destination + 6);
+}
+
+void test_copyIfReverse(){
+    test_copyIfReverse_1_digit_copying();
+    test_copyIfReverse_2_letter_copying();
 }
 
 int main() {
@@ -175,6 +265,9 @@ int main() {
     test_findNonSpaceReverse();
     test_findSpaceReverse();
     test_strcmp();
+    test_copy();
+    test_copyIf();
+    test_copyIfReverse();
 
     return 0;
 }
